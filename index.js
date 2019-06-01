d90499490786317504dc6fc99cfdef0080dfbd75
charming-reference-7654 <sydeffect1994@gmail.com>
2019-06-01 07:55:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,5 @@ function shout(string) {return string.toUpperCase()
 function whisper(string) {return string.toLowerCase()}
 
 function logShout(string){
-  console.log(string.toLowerCase());
-  logShout("HELLO")
+  console.log(string.toUpperCase());
 }
