1bd3e4507df8419f96b9bec35157ff58739504a4
charming-reference-7654 <sydeffect1994@gmail.com>
2019-06-01 08:11:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,5 +21,7 @@ function sayHiToGrandma(string){
   if (string.toUpperCase(string) === string) {
     return uppercase;
   }
-  if (string == "I love you, Grandma.")
+  if (string == "I love you, Grandma.") {
+    return love;
+  }
 }
